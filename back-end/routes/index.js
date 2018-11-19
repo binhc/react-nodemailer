5027cc7f7f91f693920853210f2c535eb972559b
Derek Bytheway <derekbtw@gmail.com>
2018-11-19 16:01:11 -0700

    Update index.js

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -25,7 +25,7 @@ router.post('/send', (req, res, next) => {
   var name = req.body.name
   var email = req.body.email
   var message = req.body.message
-  var content = `name: ${name} \n email: ${email} \n message: ${content} `
+  var content = `name: ${name} \n email: ${email} \n message: ${message} `
 
   var mail = {
     from: name,
